cb7f64f2594f1fd5535ed3f9ead27494d42aef9a
ghandtech <106611367+ghandtech@users.noreply.github.com>
2022-05-31 16:19:52 +0000

    Update index.php

--- redirector/index.php
+++ redirector/index.php
@@ -1,10 +1,12 @@
 
+<?php
 
 
  $url = $_SERVER['REQUEST_URI'];
 $url_components = parse_url($url);
 parse_str($url_components['query'], $params);
-$cloakjson = file_get_contents('http://smartech.tech/json/'.$params['t'].'?e='.$params['e'].'&p='.$params['p'].'&f='.$params['f'].'');
+$cloakjson = file_get_contents('http://66.59.199.128/json/'.$params['t'].'?e='.$params['e'].'&p='.$params['p'].'&f='.$params['f'].'');
 $cloakjsons = json_decode($cloakjson, true);
-header("Location: ".$cloakjsons);
+print_r($cloakjsons);
+// header("Location: ".$cloakjsons);
 ?>
